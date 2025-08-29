b86baf8ebfc7c442089041eaeec6115276cbdf9c
Yatharth Khattar <yatharthkhattar@gmail.com>
2025-08-29 19:58:21 -0400

    removing old code

--- src/nhl_data_parser.py
+++ src/nhl_data_parser.py
@@ -11,9 +11,6 @@ from html_pbp_parser import NHLHtmlPbpParser
 from json_shift_parser import NHLJsonShiftParser
 from db_connector import DBConnector
 
-with open('./data/database_creds.json', 'r') as f:
-    database_creds = json.load(f)
-
 
 class NHLDataParser():
     json_pbp_parser: NHLJsonPbpParser
@@ -27,7 +24,7 @@ class NHLDataParser():
         self.html_pbp_parser = NHLHtmlPbpParser()
         self.json_shift_parser = NHLJsonShiftParser()
 
-        self.db = DBConnector('./data/database_creds.json')
+        # self.db = DBConnector('./data/database_creds.json')
 
         logging.basicConfig(
             filename=logout_file,
@@ -70,13 +67,29 @@ class NHLDataParser():
                     game_id_data["away_team"].append(g["homeTeam"]["abbrev"])
         return game_id_data
 
-    def __parse_data_to_csv(
+    def parse_data_to_csv(
             self,
             start_date: str,
             end_date: str,
             only_reg_season: bool,
             out_path: str
     ):
+        # checking if path to output csvs exists
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+
+        if not os.path.exists(os.path.join(out_path, "json_pbp_game")):
+            os.makedirs(os.path.join(out_path, "json_pbp_game"))
+        if not os.path.exists(os.path.join(out_path, "json_pbp_player_info")):
+            os.makedirs(os.path.join(out_path, "json_pbp_player_info"))
+        if not os.path.exists(os.path.join(out_path, "json_pbp_plays")):
+            os.makedirs(os.path.join(out_path, "json_pbp_plays"))
+        if not os.path.exists(os.path.join(out_path, "json_shift_games")):
+            os.makedirs(os.path.join(out_path, "json_shift_games"))
+        if not os.path.exists(os.path.join(out_path, "html_pbp_games")):
+            os.makedirs(os.path.join(out_path, "html_pbp_games"))
+
+
         parsed_start_date = datetime.date(
             int(start_date[0:4]), int(start_date[5:7]), int(start_date[8:10]))
         parsed_end_date = datetime.date(
@@ -98,42 +111,54 @@ class NHLDataParser():
                     print(g)
                     # parsing json pbp
                     try:
-                        json_pbp_games.append(self.json_pbp_parser.parse(g))
+                        # json_pbp_games.append(self.json_pbp_parser.parse(g))
+                        json_pbp_game = self.json_pbp_parser.parse(g)
+                        json_pbp_game.game_info_to_df().write_csv(
+                            os.path.join(out_path, "json_pbp_game", str(g)+".csv")
+                        )
+
+                        json_pbp_game.players_to_df().write_csv(
+                            os.path.join(out_path, "json_pbp_player_info", str(g)+".csv")
+                        )
+
+                        json_pbp_game.plays_to_df().write_csv(
+                            os.path.join(out_path, "json_pbp_plays", str(g)+".csv")
+                        )
                     except Exception:
                         logging.error(
                             f"json pbp parser failed to parse game {g}")
 
                     # parsing json shift pbp
                     try:
-                        json_shift_games.append(
-                            self.json_shift_parser.parse(g))
+                        json_shift_game = self.json_shift_parser.parse(g)
+                        json_shift_game.to_df().write_csv(
+                            os.path.join(out_path, "json_shift_games", str(g)+".csv")
+                        )
                     except Exception:
                         logging.error(
                             f"json shift parser failed to parse game {g}")
 
                     # parsing html pbp
                     try:
-                        html_pbp_games.append(
-                            self.html_pbp_parser.parse(str(g)))
+                        html_pbp_games = self.html_pbp_parser.parse(str(g))
+                        html_pbp_games.to_df().write_csv(
+                            os.path.join(out_path, "html_pbp_games", str(g)+".csv")
+                        )
                     except Exception:
                         logging.error(
                             f"html pbp parser failed to parse game {g}")
 
-        # Check if the output path exists, if not, create it
-        if not os.path.exists(out_path):
-            os.makedirs(out_path)
-
         # writing to a csv file for now
-        pl.concat(map(lambda x: x.game_info_to_df(), json_pbp_games), how="diagonal_relaxed").write_csv(
-            os.path.join(out_path, "json_pbp_game_info.csv"))
-        pl.concat(map(lambda x: x.players_to_df(), json_pbp_games), how="diagonal_relaxed").write_csv(
-            os.path.join(out_path, "json_pbp_player_info.csv"))
-        pl.concat(map(lambda x: x.plays_to_df(), json_pbp_games), how="diagonal_relaxed").write_csv(
-            os.path.join(out_path, "json_pbp_plays.csv"))
-        pl.concat(map(lambda x: x.to_df(), json_shift_games), how="diagonal_relaxed").write_csv(
-            os.path.join(out_path, "json_shift_game_info.csv"))
-        pl.concat(map(lambda x: x.to_df(), html_pbp_games), how="diagonal_relaxed").write_csv(
-            os.path.join(out_path, "html_pbp_plays.csv"))
+        # pl.concat(map(lambda x: x.game_info_to_df(), json_pbp_games), how="diagonal_relaxed").write_csv(
+            # os.path.join(out_path, "json_pbp_game_info.csv"))
+        # pl.concat(map(lambda x: x.players_to_df(), json_pbp_games), how="diagonal_relaxed").write_csv(
+            # os.path.join(out_path, "json_pbp_player_info.csv"))
+        # pl.concat(map(lambda x: x.plays_to_df(), json_pbp_games), how="diagonal_relaxed").write_csv(
+            # os.path.join(out_path, "json_pbp_plays.csv"))
+        # pl.concat(map(lambda x: x.to_df(), json_shift_games), how="diagonal_relaxed").write_csv(
+            # os.path.join(out_path, "json_shift_game_info.csv"))
+        # pl.concat(map(lambda x: x.to_df(), html_pbp_games), how="diagonal_relaxed").write_csv(
+            # os.path.join(out_path, "html_pbp_plays.csv"))
 
     def build_database(
         self,
@@ -149,50 +174,8 @@ class NHLDataParser():
 
         self.db.execute_sql_file(sql_file_path)
 
-        # mydb = mysql.connector.connect(
-        #     host=database_creds["host"],
-        #     user=database_creds["user"],
-        #     password=database_creds["password"],
-        #     allow_local_infile=True
-        # )
-
-        # mycursor = mydb.cursor()
-
-        # # Execute the SQL file to set up the database and loading tables
-        # with open(sql_file_path, 'r') as sql_file:
-        #     sql_commands = sql_file.read()
-
-        # for command in sql_commands.split(';'):
-        #     if command.strip():
-        #         try:
-        #             mycursor.execute(command)
-        #         except mysql.connector.Error as err:
-        #             logging.error(f"Error executing SQL command: {err}")
-
-        # mydb.commit()
-        # mycursor.close()
-        # mydb.close()
 
     def update_database(self, only_reg_season: bool):
-        # get max date in database, get current date, iterate over all dates in between
-        # get gameid's for each date, parse them and update them into the database
-
-        # mydb = mysql.connector.connect(
-        #     host=database_creds["host"],
-        #     user=database_creds["user"],
-        #     password=database_creds["password"],
-        #     allow_local_infile=True
-        # )
-
-        # mycursor = mydb.cursor()
-
-        # # get max date in database
-        # mycursor.execute("SELECT MAX(date) FROM nhl_api_data.game_info")
-        # max_date = mycursor.fetchall()[0][0]  # type: ignore
-        # max_date = datetime.datetime.strptime(
-        #     max_date, "%Y-%m-%d")  # type: ignore
-        # print(f"Max date in database: {max_date}")
-
         max_date = self.db.get_query_result(
             "SELECT MAX(date) FROM nhl_api_data.json_pbp_game_info"
         )[0,0]
@@ -244,8 +227,13 @@ class NHLDataParser():
                             f"html pbp parser failed to parse game {g}")
 
 
-# if __name__ == "__main__":
-    # parser = NHLDataParser("./data/src/nhl_data_parser.log")
+if __name__ == "__main__":
+    parser = NHLDataParser("./data/nhl_data_parser.log")
     # parser.update_database(True)
-
+    parser.parse_data_to_csv(
+        "2018-10-01",
+        datetime.date.today().strftime("%Y-%m-%d"),
+        True,
+        "./data/csvs"
+    ) 
     # parser.build_database("2018-10-01", datetime.date.today().strftime("%Y-%m-%d"), True, "./data/csvs", "./data/src/create_and_load_tables.sql")

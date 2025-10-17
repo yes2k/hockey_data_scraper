ab8d7b48dc60443d9e8662e3d6a100743d6684cb
Yatharth Khattar <yatharthkhattar@gmail.com>
2025-10-17 17:09:22 -0400

    commenting out old db code

--- src/db_connector.py
+++ src/db_connector.py
@@ -7,14 +7,14 @@ class DBConnector:
     def __init__(self, db_config_path: str):
         with open(db_config_path, 'r') as f:
             database_creds = json.load(f)
-        
+
         # try:
-        self.mydb = mysql.connector.connect(
-            host="test-mysql-db",
-            user="root",
-            password="password",
-            allow_local_infile=True
-        )
+        # self.mydb = mysql.connector.connect(
+        #     host="test-mysql-db",
+        #     user="root",
+        #     password="password",
+        #     allow_local_infile=True
+        # )
         # print("Connection successful.")
         # except mysql.connector.Error as err:
         #     print(f"Error: {err}")
@@ -30,14 +30,14 @@ class DBConnector:
         except mysql.connector.Error as err:
             print("connection error")
             raise err
-    
+
     def execute_sql_file(self, sql_file_path: str):
         mycursor = self.mydb.cursor()
-        
+
         # Execute the SQL file to set up the database and loading tables
         with open(sql_file_path, 'r') as sql_file:
             sql_script = sql_file.read()
-        
+
         for statement in sql_script.split(';'):
             if statement.strip():
                 try:
@@ -45,15 +45,15 @@ class DBConnector:
                 except mysql.connector.Error as err:
                     print(f"Error executing SQL statement: {statement.strip()[:100]}...")
                     print(f"MySQL Error: {err}")
-        
+
         self.mydb.commit()
         mycursor.close()
 
-    
+
     def get_query_result(self, query: str) -> pl.DataFrame:
         db_cursor = self.mydb.cursor()
         db_cursor.execute(query)
-        
+
         rows = db_cursor.fetchall()
         if db_cursor.description is not None:
             columns = [desc[0] for desc in db_cursor.description]
@@ -62,7 +62,7 @@ class DBConnector:
             df = pl.DataFrame()
         db_cursor.close()
         return df
-    
+
 
     def load_parquet_to_mysql(self, parquet_path: str, table_name: str):
         # Read Parquet file
@@ -86,8 +86,6 @@ class DBConnector:
         insert_sql = f"INSERT INTO {table_name} ({','.join(columns)}) VALUES ({placeholders})"
 
         for row in df.iter_rows():
-            print(row)
             cursor.execute(insert_sql, row)
         self.mydb.commit()
         cursor.close()
-        

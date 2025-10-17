b2ed9bd28c4e588e4323f53fb494203ecf2c6b86
Yatharth Khattar <yatharthkhattar@gmail.com>
2025-10-17 17:00:38 -0400

    remove cron job

--- startup.sh
+++ startup.sh
@@ -2,9 +2,4 @@
 python ./src/nhl_data_parser.py build_from_csv_backup --csv_path ./csvs --sql_file_path ./src/create_and_load_tables.sql
 
 # initial update
-python ./src/nhl_data_parser.py --update_database 
-
-# adding cron tab
-chmod 0644 /etc/cron.d/nhl_cron && crontab /etc/cron.d/nhl_cron
-
-cron -f
+python ./src/nhl_data_parser.py --update_database
